09ee77de5153d5b5765498b873838aa2fc73a6a0
ZZAME <92978598+DEVZZAME@users.noreply.github.com>
2022-07-20 04:17:55 +0900

    modify calendar

--- study/JQuery/js/calendar.js
+++ study/JQuery/js/calendar.js
@@ -1,9 +1,13 @@
-// $("td").on("click", function (e) {
-//   $("#date").val(`2022년 7월 ${this.innerText}일`);
-//   $("button").off("click").on("click", function() {
-//       $(e.currentTarget).append($("<div>").text($("#content").val()));
-//     });
-// })
+$("td").on("click", function (e) {
+  $("#date").val(`2022년 7월 ${this.innerText}일`);
+  $("button").off("click").on("click", function () {
+    let div = $("<div>").text($("#content").val());
+    div.on("click", function () {
+      div.remove();
+    })
+    $(e.currentTarget).append(div);
+  });
+})
 
 // $("button").unbind("click");
 // $("button").bind("click", function () {
@@ -11,22 +15,24 @@
 //   e.currentTarget = null;
 // });
 
-const date = document.querySelector("#date");
-const content = document.querySelector("#content");
-const btn = document.querySelector("button");
-const table = document.querySelector("table");
 
-table.addEventListener("click", function (e) {
-  date.value = `2022년 7월 ${e.target.textContent}일`;
-  target = e.target;
-}); //table.addEventListener
 
-function writeSchedule() {
-  const dv = document.createElement("div");
-  dv.textContent = content.value;
-  target.after(dv);
+// const date = document.querySelector("#date");
+// const content = document.querySelector("#content");
+// const btn = document.querySelector("button");
+// const table = document.querySelector("table");
 
-  dv.addEventListener("click", function () {
-    dv.remove();
-  });//dv.addEventListener
-} //writeSchedule()
+// table.addEventListener("click", function (e) {
+//   date.value = `2022년 7월 ${e.target.textContent}일`;
+//   target = e.target;
+// }); //table.addEventListener
+
+// function writeSchedule() {
+//   const dv = document.createElement("div");
+//   dv.textContent = content.value;
+//   target.after(dv);
+
+//   dv.addEventListener("click", function () {
+//     dv.remove();
+//   });//dv.addEventListener
+// } //writeSchedule()
